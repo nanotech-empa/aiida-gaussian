38d75f8d98cfffd5a8931baaa5a8757542b7d232
Kristjan Eimre <kristjaneimre@gmail.com>
2020-02-19 17:18:49 +0100

    example with the LSF scheduler

---
+++ examples/example_LSF.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
+"""Run simple DFT calculation"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+
+import os
+import sys
+import ase.build
+import click
+
+from aiida.engine import run
+from aiida.orm import (Code, Dict, SinglefileData, StructureData)
+from aiida.common import NotExistent, InputValidationError
+from aiida.plugins import CalculationFactory
+
+import pymatgen as mg
+
+GaussianCalculation = CalculationFactory('gaussian')
+
+
+def example_dft(gaussian_code):
+    """Run simple DFT calculation"""
+
+    print("Testing Gaussian Input Creation")
+
+    pwd = os.path.dirname(os.path.realpath(__file__))
+
+    # structure
+    structure = StructureData(pymatgen_molecule=mg.Molecule.from_file('./ch4.xyz'))
+
+    num_cores = 1
+    memory_mb = 1000
+
+    # parameters
+    parameters = Dict(
+        dict={
+            'link0_parameters': {
+                '%chk':'mychk.chk',
+                '%mem':"%dMB" % memory_mb,
+                '%nprocshared': str(num_cores),
+            },
+            'functional':'PBE1PBE',
+            'basis_set':'6-31g',
+            'route_parameters': {
+                'nosymm': None,
+                'Output':'WFX'
+            },
+            'input_parameters': {
+                'output.wfx':None
+            },
+        })
+
+    # Construct process builder
+
+    builder = GaussianCalculation.get_builder()
+
+    builder.structure = structure
+    builder.parameters = parameters
+    builder.code = gaussian_code
+
+    #builder.metadata.options.resources = {
+    #    "num_machines": 1,
+    #    "num_mpiprocs_per_machine": num_cores,
+    #    "tot_num_mpiprocs": num_cores,
+    #}
+    builder.metadata.options.resources = {
+        "tot_num_mpiprocs": 1,
+        "num_machines": 1,
+    }
+
+    builder.metadata.options.max_memory_kb = memory_mb*1024
+    builder.metadata.options.custom_scheduler_commands = "#BSUB -R \"rusage[mem=%d,scratch=%d]\"" % (
+        int(memory_mb/num_cores*1.25),
+        int(memory_mb/num_cores*2.15*2)
+    )
+
+    builder.metadata.options.max_wallclock_seconds = 1 * 3 * 60
+    #builder.metadata.dry_run = True
+    #builder.metadata.store_provenance = False
+
+    print("Submitted calculation...")
+    run(builder)
+
+
+@click.command('cli')
+@click.argument('codelabel')
+def cli(codelabel):
+    """Click interface"""
+    try:
+        code = Code.get_from_string(codelabel)
+    except NotExistent:
+        print("The code '{}' does not exist".format(codelabel))
+        sys.exit(1)
+    example_dft(code)
+
+
+if __name__ == '__main__':
+    cli()  # pylint: disable=no-value-for-parameter

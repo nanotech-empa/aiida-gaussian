2c43167bc5d66c74d599cec1835e8789bfa00989
Kristjan Eimre <kristjaneimre@gmail.com>
2020-02-20 18:39:19 +0100

    Added cubegen plugin

---
+++ aiida_gaussian/calculations/cubegen.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""Gaussian input plugin."""
+from __future__ import absolute_import
+import os
+from shutil import copyfile, copytree
+import six
+from six.moves import map, range
+
+from aiida.orm import Dict, FolderData, List, RemoteData, SinglefileData, Str, Int, Bool
+from aiida.common import CalcInfo, CodeInfo, InputValidationError
+from aiida.engine import CalcJob
+from aiida.plugins import DataFactory
+
+
+class CubegenCalculation(CalcJob):
+    """
+    Plugin to run the cubegen utility
+
+    Example:
+
+    parameters = {
+        "homo-5": {
+            "kind": "AMO=16",
+            "npts": -2,
+        },
+        "spin": {
+            "kind": "Spin=SCF",
+            "npts": 0,
+        },
+    }
+    Each key corresponds to one produced cube.
+    key specifies the name of the output node
+    """
+
+    _DEFAULT_INPUT_FILE = "aiida.fchk"
+    _PARENT_FOLDER_NAME = "parent_calc"
+
+    @classmethod
+    def define(cls, spec):
+        super(CubegenCalculation, cls).define(spec)
+
+        spec.input("parameters", valid_type=Dict, required=True, help='dictionary containing entries for cubes to be printed.')
+        spec.input('parent_calc', valid_type=RemoteData, required=True, help='the folder of a containing the .fchk')
+
+        spec.input('retrieve_cubes', valid_type=Bool, required=False, default=Bool(False), help='should the cube be retrieved?')
+        spec.input("gauss_memdef", valid_type=Int, required=False, default=Int(1024), help="Set the GAUSS_MEMDEF env variable to set the max memory in MB.")
+
+        # Turn mpi off by default
+        spec.input('metadata.options.withmpi', valid_type=bool, default=False)
+
+    # --------------------------------------------------------------------------
+    # pylint: disable = too-many-locals
+    def prepare_for_submission(self, folder):
+        
+         # create calculation info
+        calcinfo = CalcInfo()
+        calcinfo.uuid = self.uuid
+        calcinfo.codes_info = []
+        calcinfo.retrieve_list = []
+
+        for key, params in self.inputs.parameters.get_dict().items():
+            
+            cube_name = key+".cube"
+            kind_str = params["kind"]
+            npts = params["npts"]
+
+            # create code info 
+            codeinfo = CodeInfo()
+
+            codeinfo.cmdline_params = []
+            codeinfo.cmdline_params.append(str(self.inputs.metadata.options.resources['tot_num_mpiprocs']))
+            codeinfo.cmdline_params.append(kind_str)
+            codeinfo.cmdline_params.append(self._PARENT_FOLDER_NAME + "/" + self._DEFAULT_INPUT_FILE)
+            codeinfo.cmdline_params.append(cube_name)
+            codeinfo.cmdline_params.append(str(npts))
+
+            codeinfo.code_uuid = self.inputs.code.uuid
+            codeinfo.withmpi = self.inputs.metadata.options.withmpi
+
+            calcinfo.codes_info.append(codeinfo)
+
+            if self.inputs.retrieve_cubes.value:
+                calcinfo.retrieve_list.append(cube_name)
+
+        extra_prepend = "\nexport GAUSS_MEMDEF=%dMB\n" % self.inputs.gauss_memdef
+        if not hasattr(calcinfo, 'prepend_text') or not calcinfo.prepend_text:
+            calcinfo.prepend_text = extra_prepend
+        else:
+            calcinfo.prepend_text += extra_prepend
+
+         # symlink or copy to parent calculation
+        calcinfo.remote_symlink_list = []
+        calcinfo.remote_copy_list = []
+        comp_uuid = self.inputs.parent_calc.computer.uuid
+        remote_path = self.inputs.parent_calc.get_remote_path()
+        copy_info = (comp_uuid, remote_path, self._PARENT_FOLDER_NAME)
+        if self.inputs.code.computer.uuid == comp_uuid:  # if running on the same computer - make a symlink
+            # if not - copy the folder
+            calcinfo.remote_symlink_list.append(copy_info)
+        else:
+            calcinfo.remote_copy_list.append(copy_info)
+        
+
+        return calcinfo
---
+++ examples/example_cubegen_LSF.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
+"""Run simple DFT calculation"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+
+import os
+import sys
+import ase.build
+import click
+
+from aiida.engine import run
+from aiida.orm import Code, Dict, SinglefileData, StructureData, load_node, Bool
+from aiida.common import NotExistent, InputValidationError
+from aiida.plugins import CalculationFactory
+
+CubegenCalculation = CalculationFactory('gaussian.cubegen')
+
+
+def example(code):
+    """Run simple DFT calculation"""
+
+
+    builder = CubegenCalculation.get_builder()
+    builder.parent_calc = load_node(181).outputs.remote_folder
+    builder.code = code
+
+    builder.parameters = Dict(dict = {
+        "homo": {
+            "kind": "MO=Homo",
+            "npts": -2,
+        },
+        "density": {
+            "kind": "Density=SCF",
+            "npts": -2,
+        },
+    })
+
+    builder.retrieve_cubes = Bool(True)
+
+    builder.metadata.options.resources = {
+        "tot_num_mpiprocs": 1,
+        "num_machines": 1,
+    }
+
+    builder.metadata.options.max_wallclock_seconds = 1 * 5 * 60
+
+    print("Submitted calculation...")
+    run(builder)
+
+
+@click.command('cli')
+@click.argument('codelabel')
+def cli(codelabel):
+    """Click interface"""
+    try:
+        code = Code.get_from_string(codelabel)
+    except NotExistent:
+        print("The code '{}' does not exist".format(codelabel))
+        sys.exit(1)
+    example(code)
+
+
+if __name__ == '__main__':
+    cli()  # pylint: disable=no-value-for-parameter
